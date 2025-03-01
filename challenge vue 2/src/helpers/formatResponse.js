baf3840079d6faed0b433980e141d08b2b845656
Nicolas Toconas <ntoconas@lexim.solutions>
2025-03-01 17:34:24 -0300

    Punto 2 ordenar por vocales

--- challenge vue 2/src/helpers/formatResponse.js
+++ challenge vue 2/src/helpers/formatResponse.js
@@ -1,10 +1,16 @@
+const vowelsNum = (str) => (str.match(/[aeiou]/gi) || []).length;
+
 export const formatResponse = (data) => {
-    return data.map((item) => ({
+  return data
+    .map((item) => ({
       image: item.image?.original || "",
       name: item.name || "",
       type: item.type || "",
       network: item.network?.name || "",
       officialSite: item.officialSite || "",
       genres: item.genres || [],
-    }));
-  };
+      vowels: vowelsNum(item.name || ""),
+    }))
+    .sort((a, b) => a.vowels - b.vowels || a.name.localeCompare(b.name))
+    .map(({ vowels, ...rest }) => rest);
+};
